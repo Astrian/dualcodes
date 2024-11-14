f73c514eee62b3e26f829a7cfe9fa6995d2346e3
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-11-14 16:47:41 +1100

    Fixed a bug will cause the changes not effected

--- packages/frontend/src/pages/AccountDetail/index.tsx
+++ packages/frontend/src/pages/AccountDetail/index.tsx
@@ -33,19 +33,25 @@ function AccountDetail() {
 	async function savechanges() {
 		const accountsLS = localStorage.getItem('tfa_accounts')
 		if (!accountsLS) return
-		const accounts = JSON.parse(accountsLS) as Account[]
+		let accounts = JSON.parse(accountsLS) as Account[]
 		console.log(accounts)
 		for (let i in accounts) {
+			console.log(accounts[i].id, accountId)
 			if (accounts[i].id !== accountId) continue
-			accounts[i].website = websiteField
-			accounts[i].name = nameField
-			accounts[i].secret = secretField
+			console.log('hit')
+			const newAccountInfo = {
+				id: accountId,
+				website: websiteField,
+				name: nameField,
+				secret: secretField
+			}
+			accounts[i] = newAccountInfo
+			localStorage.setItem('tfa_accounts', JSON.stringify(accounts))
+			syncToServer()
+			toast(t('ACCOUNTDETAIL_SAVEOPS_SUCCESS'), {type: 'success'})
+			// window.history.back()
 			break
 		}
-		localStorage.setItem('tfa_accounts', JSON.stringify(accounts))
-		syncToServer()
-		toast(t('ACCOUNTDETAIL_SAVEOPS_SUCCESS'), {type: 'success'})
-		window.history.back()
 	}
 
 	async function deleteAccount() {
@@ -68,21 +74,21 @@ function AccountDetail() {
 				<PilledTableCell>
 					<div className="font-semibold">{t('ACCOUNTDETAIL_FIELDS_WEBSITE_TITLE')}</div>
 					<div>
-						<input type='text' className='w-full outline-none bg-transparent' placeholder='Awesome website' defaultValue={websiteField} />
+						<input type='text' className='w-full outline-none bg-transparent' placeholder='Awesome website' defaultValue={websiteField} onChange={e => setWebsiteField(e.target.value)} />
 					</div>
 				</PilledTableCell>
 
 				<PilledTableCell>
 					<div className="font-semibold">{t('ACCOUNTDETAIL_FIELDS_NAME_TITLE')}</div>
 					<div>
-						<input type='text' className='w-full outline-none bg-transparent' placeholder='John Appleseed' defaultValue={nameField} />
+						<input type='text' className='w-full outline-none bg-transparent' placeholder='John Appleseed' defaultValue={nameField} onChange={e => setNameField(e.target.value)} />
 					</div>
 				</PilledTableCell>
 
 				<PilledTableCell>
 					<div className="font-semibold">{t('ACCOUNTDETAIL_FIELDS_2FASECRECT_TITLE')}</div>
 					<div className='flex'>
-						<input type={displaySecret ? 'text' : 'password'} className='w-full outline-none bg-transparent' placeholder='Awesome website' defaultValue={secretField} />
+						<input type={displaySecret ? 'text' : 'password'} className='w-full outline-none bg-transparent' placeholder='ABCD 1234 ...' defaultValue={secretField} onChange={e => setSecretField(e.target.value)} />
 						<button onClick={() => setDisplaySecret(!displaySecret)}>
 							{displaySecret ? <Icon path={mdiEyeOff} size={1} /> : <Icon path={mdiEye} size={1} />}
 						</button>
